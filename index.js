faa2de75a064f14d95ac5ecc9bac71b858db4f31
Miguel Nazario <mnazario35@gmail.com>
2022-01-22 19:06:46 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+*/
+
+const newArray = [];
+
+function writeCards(array, event) {
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(num) {
+    let countDown = num;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
